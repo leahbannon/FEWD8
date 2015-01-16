b0e6d1703369483e9ca79506b475593769f18b32
Leah Bannon <leahbannon@gmail.com>
2015-01-16 16:11:33 -0500

    more homework!

--- public_html/class07/js-basics-starter/traffic_light/js/main.js
+++ public_html/class07/js-basics-starter/traffic_light/js/main.js
@@ -1,12 +1,25 @@
+//create the function clearLights, look in the index file for the id stopLight and style it black.
 function clearLights() {
     document.getElementById('stopLight').style.backgroundColor = "black";
+    document.getElementById('slowLight').style.backgroundColor = "black";
+    document.getElementById('goLight').style.backgroundColor = "black";
 }
 
+//create the function illuminateRed, first make everything black with the clearLights function, then style red
 function illuminateRed() {
     clearLights();
     document.getElementById('stopLight').style.backgroundColor = "red";
 }
+function illuminateYellow() {
+  clearLights();
+  document.getElementById('slowLight').style.backgroundColor = "yellow";
+}
+function illuminateGreen() {
+  clearLights();
+  document.getElementById('goLight').style.backgroundColor = "green";
+}
 
+//go in the index file and find the id stopButton and when you click on it, implment to the illuminateRed function
 document.getElementById('stopButton').onclick = illuminateRed;
-
-//when you click on slow, the stop should go back to black and the slow should become yellow
+document.getElementById('slowButton').onclick = illuminateYellow;
+document.getElementById('goButton').onclick = illuminateGreen;
